d416fe7f234db2c1539694f27a46df2426e36501
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-01 22:34:39 +0530

    access control for product ownership

--- ProductSupplyChain/contracts/ProductSupplyChain.sol
+++ ProductSupplyChain/contracts/ProductSupplyChain.sol
@@ -1,4 +1,40 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.23;
 
-contract ProductSupplyChain {}
+contract Ownable {
+    mapping(uint256 => mapping(address => bool)) public OWNER;
+
+    modifier onlyOwner(uint256 productId) {
+        require(OWNER[productId][msg.sender], "Not Owner");
+        _;
+    }
+
+    function transferOwnership(
+        address newOwner,
+        uint256 productId
+    ) external onlyOwner(productId) {
+        require(newOwner != address(0), "Invalid newOwner address");
+        OWNER[productId][newOwner] = true;
+    }
+}
+
+contract ProductSupplyChain is Ownable {
+    address public administrator;
+    // Product struct with properties like productId, name, currentOwner, price, and state.
+    struct Product {
+        uint256 productId;
+        string name;
+        address currentOwner;
+        uint256 price;
+    }
+    mapping(uint256 => Product) public STORAGE;
+
+    modifier onlyAdministrator() {
+        require(msg.sender == administrator, "Not Administrator");
+        _;
+    }
+
+    constructor() {
+        administrator = msg.sender;
+    }
+}
